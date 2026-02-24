d7ec8f7cf265901f33e0c88f9b71c21f7e9a6937
ead <ead@academico.local>
2026-02-24 19:42:47 -0300

    first commit

---
+++ Dados do sistema de Cadastro/LivrosJava/src/test/java/com/projeto/LivrosJava/LivrosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.LivrosJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
